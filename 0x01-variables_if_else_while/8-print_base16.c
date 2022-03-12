e2caa64e21641ea0a12a3872babe7e4a10710323
matiworku <matiworku0721@gmail.com>
2022-03-11 17:47:07 -0800

    Task 8 and 9 completed

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - HexaDecimal numbers in lowercase
+ *
+ * This function prints numbers of base16 in lowercase
+ * Return: 0 always
+ */
+int main(void)
+{
+	char ch;
+	int n;
+
+	for (n = 48; n <= 58; n++)
+		putchar(n);
+	for (ch = 'A'; ch <= 'Z'; ch++)
+		putchar(ch);
+	putchar('\n');
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - print single digit number using putchar
+ *
+ * This function prints singlr digit numberd using putchar and
+ * separating them using comma and space
+ * Return: 0 always
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 48; i < 58; i++)
+	{
+		putchar(i);
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
