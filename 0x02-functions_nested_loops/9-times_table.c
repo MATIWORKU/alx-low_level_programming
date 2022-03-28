ed7b49d2edc5d3941d7f5fa5b84583a197a3d333
matiworku <matiworku0721@gmail.com>
2022-03-28 07:59:46 -0700

    Errors in task 8 and 9 fixed

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * times_table- creates a multiple table of 9
@@ -7,15 +8,32 @@
 void times_table(void)
 {
 	int i;
-	int j;
+	int j , product;
 
 	for (i = 0 ; i < 10 ; i++)
 	{
 		for (j = 0 ; j < 10 ; j++)
 		{
-			_putchar((i * j));
-			_putchar(',');
-			_putchar(' ');
+			product = i * j;
+			if (j != 0)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+			if (j == 0)
+			{
+				_putchar('0');
+			}
+			else if (product >= 10)
+			{
+				_putchar((product / 10) + '0');
+				_putchar((product % 10) + '0');
+			}
+			else if ((product < 10) && (j != 0))
+			{
+				_putchar(' ');
+				_putchar((product % 10) + '0');
+			}
 		}
 		_putchar('\n');
 	}
